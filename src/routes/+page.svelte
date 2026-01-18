1295138cbe11f7bb75f8585604a9c14b8d9d5af2
SharmaDevanshu089 <sharma.devanshu089@gmail.com>
2026-01-19 01:31:44 +0530

    Finish Update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,22 @@
+<script lang="ts">
+    import gsap from "gsap";
+    import { onMount } from "svelte";
+
+    let Head1,Head2,Test1,Test2,Button1:any;
+    let timline = gsap.timeline();
+    onMount(()=> 
+    timline.from(Head1,{ x: -1000, duration: 0.25}).from(Head2,{ x: -1000, duration:0.25}).from(Test1,{x: -1000,duration:0.25}).from(Test2, {x:-1000, duration:0.25}));
+</script>
 <svelte:head>
     <title>
         Test 
     </title>
 </svelte:head>
-<input type="text">
+<h1 bind:this={Head1}>Name</h1>
+<input bind:this={Test1} type="text" value="Name">
 <br>
-<input type="text">
+<h1 bind:this={Head2}>Surname</h1>
+<input bind:this={Test2} type="text" value="Surname">
 <br>
-<button>Submit</button>
+<h1>
+<button>Submit</button></h1>
